cdf4d74c1ce3ef5b2b6bb9731e63914988402514
joaquin <joaquinb@takeoffmedia.com>
2021-09-11 22:42:26 -0300

    [Mainpage] Mainpage created. Routing implemented.

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  {
+    path: "login", component: LoginComponent
+  },
+  { path: "main", loadChildren: () => import('./components/main/main.module').then(x => x.MainModule) },
+  { path: "**", redirectTo: "login", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
