2f8aae8e454230122f8c2d777cb824b4b0679ff8
joaquin <joaquinb@takeoffmedia.com>
2021-10-05 20:26:48 -0300

    [APP] Average price by destination chart implemented.

--- angular-app/src/app/components/dashboard/grafica-precios-destino/grafica-precios-destino.component.ts
+++ angular-app/src/app/components/dashboard/grafica-precios-destino/grafica-precios-destino.component.ts
@@ -4,9 +4,44 @@ import {
   OnChanges,
   SimpleChanges,
   Input,
+  ViewChild,
 } from '@angular/core';
 import { Package } from 'src/app/interfaces/package';
 import { Sell } from 'src/app/interfaces/sell';
+
+import {
+  ApexChart,
+  ApexAxisChartSeries,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexLegend,
+  ApexGrid,
+} from 'ng-apexcharts';
+
+type ApexXAxis = {
+  type?: 'category' | 'datetime' | 'numeric';
+  categories?: any;
+  labels?: {
+    style?: {
+      colors?: string | string[];
+      fontSize?: string;
+    };
+  };
+};
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  grid: ApexGrid;
+  colors: string[];
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-grafica-precios-destino',
@@ -16,14 +51,61 @@ import { Sell } from 'src/app/interfaces/sell';
 export class GraficaPreciosDestinoComponent implements OnChanges {
   @Input() sells: Sell[] = [];
   @Input() packages: Package[] = [];
+  destinations: Array<any> = [];
+  prices: Array<any> = [];
 
-  constructor() {}
+  @ViewChild('chart-personas-destino', { static: false })
+  chart!: ChartComponent;
+  public chartOptions!: Partial<ChartOptions> | any;
 
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Pasajeros por destino',
+          data: [],
+        },
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      xaxis: {
+        categories: [],
+      },
+      yaxis: {
+        title: {
+          text: 'Pasajeros',
+        },
+      },
+    };
+  }
   ngOnChanges(changes: SimpleChanges) {
     if (this.sells.length > 0 && this.packages.length > 0) {
-      this.loadGraph();
+      this.loadSells();
     }
   }
 
-  loadGraph() {}
+  loadSells() {
+    this.packages.forEach((p) => {
+      const avgPrice = (p.precio_mayor + p.precio_menor) / 2;
+      this.prices.push(avgPrice);
+      this.destinations.push(p.nombre);
+    });
+
+    this.chartOptions.series = [
+      {
+        data: this.prices,
+      },
+    ];
+    this.chartOptions.xaxis = { categories: this.destinations };
+  }
 }
